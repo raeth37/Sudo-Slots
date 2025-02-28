a86e309c2c72a17f98afb899f1cd6726f4d15235
the8bitbyte <133459899+the8bitbyte@users.noreply.github.com>
2025-02-28 16:36:47 -0500

    Created java project using maven.

---
+++ src/main/java/org/sudo-slots/SudoSlot.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.sudo-slots;
+
+/**
+ *
+ * @author the8bitbyte
+ */
+public class SudoSlot {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
